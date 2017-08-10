8da8b328a8f744d3251ced1dd71926ff7964e358
Rajat Kumar Sanu <rajatkumar.sanu@varian.com>
2017-08-10 14:48:16 +0530

    deleted report and temp folder
    deleted report and temp folder

--- .temp/typescript/common.js
+++
@@ -1,8 +0,0 @@
-/// <reference path="../../typings/index.d.ts" />
-var Common = (function () {
-    function Common() {
-    }
-    Common.url = 'http://www.buybooksindia.com/';
-    return Common;
-})();
-exports.Common = Common;
